9283c433f524b8fa3272c3e6bc5b848661c2a573
Raouf Satto <raoufsato999@gmail.com>
2021-12-28 19:39:48 +0300

    add validations for forms

--- client/src/components/BoardForm/BoardForm.jsx
+++ client/src/components/BoardForm/BoardForm.jsx
@@ -1,10 +1,33 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "./boardForm.scss";
 import axios from "axios";
 
 export default function BoardForm() {
 	const name = useRef();
+	const [titleValid, setTitleValid] = useState({
+		touched: false,
+		isValid: false,
+		errMsg: "",
+	});
 
+	// input validation
+	const checkTitleValidation = (event) => {
+		const val = event.target.value.trim();
+		let valids = { ...titleValid };
+		valids.touched = true;
+		if (val.length <= 0) {
+			valids.isValid = false;
+			valids.errMsg = "Board name is Required";
+		} else if (val.split(" ").length < 2) {
+			valids.isValid = false;
+			valids.errMsg = "Board name must be more than 2 words";
+		} else {
+			valids.isValid = true;
+			valids.errMsg = "";
+		}
+		setTitleValid({ ...valids });
+		console.log(valids);
+	};
 	// create board
 	const submitHandler = async (e) => {
 		e.preventDefault();
@@ -14,13 +37,20 @@ export default function BoardForm() {
 		try {
 			await axios.post("/boards", newBoard);
 			window.location.reload();
-		} catch (err) {}
+		} catch (err) {
+			console.log(err);
+		}
 	};
-
 	return (
 		<form className="form" onSubmit={submitHandler}>
 			<div className="row">
-				<input type="text" ref={name} placeholder="Board name.." />
+				<input
+					type="text"
+					ref={name}
+					placeholder="Board name.."
+					onBlur={checkTitleValidation}
+				/>
+				{titleValid.errMsg ? <small>{titleValid.errMsg}</small> : ""}
 			</div>
 
 			<div className="submit">
--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -1,13 +1,14 @@
 import React, { useRef, useState } from "react";
 import Modal from "react-modal";
 import axios from "axios";
+
 // give style to modal
 const customStyles = {
 	content: {
 		top: "50%",
 		left: "50%",
 		width: "50%",
-		height: "400px",
+		height: "450px",
 		right: "auto",
 		bottom: "auto",
 		display: "flex",
@@ -28,7 +29,54 @@ export function Modall({ slug }) {
 
 	// to open/close modal
 	const [modalIsOpen, setIsOpen] = useState(false);
+	const [titleValid, setTitleValid] = useState({
+		touched: false,
+		isValid: false,
+		errMsg: "",
+	});
+	const [bodyValid, setBodyValid] = useState({
+		touched: false,
+		isValid: false,
+		errMsg: "",
+	});
 
+	// title input validation
+	const checkTitleValidation = (event) => {
+		const val = event.target.value.trim();
+		let valids = { ...titleValid };
+		valids.touched = true;
+		if (val.length <= 0) {
+			valids.isValid = false;
+			valids.errMsg = "Card title is Required";
+		} else if (val.split(" ").length < 2) {
+			valids.isValid = false;
+			valids.errMsg = "Card title must be more than 3 words";
+		} else {
+			valids.isValid = true;
+			valids.errMsg = "";
+		}
+		setTitleValid({ ...valids });
+		console.log(valids);
+	};
+
+	// title input validation
+	const checkBodyValidation = (event) => {
+		const val = event.target.value.trim();
+		let valids = { ...bodyValid };
+		valids.touched = true;
+		if (val.length <= 0) {
+			valids.isValid = false;
+			valids.errMsg = "Card body is Required";
+		} else if (val.split(" ").length < 6) {
+			valids.isValid = false;
+			valids.errMsg = "Card body must be more than 6 words";
+		} else {
+			valids.isValid = true;
+			valids.errMsg = "";
+		}
+		setBodyValid({ ...valids });
+		console.log(valids);
+	};
 	// create task
 	const submitHandler = async (e) => {
 		e.preventDefault();
@@ -66,11 +114,16 @@ export function Modall({ slug }) {
 					style={customStyles}
 				>
 					<h2>Kanban</h2>
-					<form className="formBottom" onSubmit={submitHandler}>
+					<form
+						className="formBottom"
+						style={{ width: "100%" }}
+						onSubmit={submitHandler}
+					>
 						<div className="formOptions">
 							<input
 								placeholder={"add title"}
 								className="shareInput"
+								onBlur={checkTitleValidation}
 								ref={title}
 								style={{
 									width: "90%",
@@ -79,6 +132,14 @@ export function Modall({ slug }) {
 									fontSize: "15px",
 								}}
 							/>
+							<div
+								className="row"
+								style={{
+									margin: "10px",
+								}}
+							>
+								{titleValid.errMsg ? <small>{titleValid.errMsg}</small> : ""}
+							</div>
 
 							<input
 								placeholder={"add body"}
@@ -90,7 +151,17 @@ export function Modall({ slug }) {
 									padding: "15px",
 									fontSize: "15px",
 								}}
+								onBlur={checkBodyValidation}
 							/>
+							<div
+								className="row"
+								style={{
+									margin: "10px",
+								}}
+							>
+								{bodyValid.errMsg ? <small>{bodyValid.errMsg}</small> : ""}
+							</div>
+
 							<input
 								type="color"
 								ref={color}
